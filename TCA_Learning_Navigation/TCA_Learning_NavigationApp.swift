4d173d0e0580083053643d086cb3e5cfa4e9635d
ZachMommaerts <zmommaerts@gmail.com>
2024-03-28 22:08:50 -0500

    Initial Commit

---
+++ TCA_Learning_Navigation/TCA_Learning_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Learning_NavigationApp.swift
+//  TCA_Learning_Navigation
+//
+//  Created by Zach Mommaerts on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Learning_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
